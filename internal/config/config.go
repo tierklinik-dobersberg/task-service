44155694e7ede85aebd8125a41735137bd84916f
Patrick Pacher <patrick.pacher@gmail.com>
2024-09-05 09:53:19 +0200

    Do not mark IDM_URL environment variable as required

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,7 +10,7 @@ import (
 type Config struct {
 	ListenAddress      string   `env:"LISTEN_ADDRESS, default=:8080"`
 	AdminListenAddress string   `env:"ADMIN_LISTEN_ADDRESS, default=:8081"`
-	IdmURL             string   `env:"IDM_URL, required"`
+	IdmURL             string   `env:"IDM_URL"`
 	AllowedOrigins     []string `env:"ALLOWED_ORIGINS, default=*"`
 	MongoDBURL         string   `env:"MONGO_URL"`
 	MongoDatabaseName  string   `env:"MONGO_DATABASE, default=customer-service"`
